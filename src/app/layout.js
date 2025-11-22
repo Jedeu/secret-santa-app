1660ea9634d02fde041b153aa8fcf2043a17df72
Jed Piezas <jed.piezas@gmail.com>
2025-11-21 19:51:26 -0800

    Remove next-auth import that is no longer used

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,12 @@
 'use client';
 import './globals.css';
-import { SessionProvider } from 'next-auth/react';
+
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en" suppressHydrationWarning>
             <body>
-                <SessionProvider>
-                    {children}
-                </SessionProvider>
+                {children}
             </body>
         </html>
     );
